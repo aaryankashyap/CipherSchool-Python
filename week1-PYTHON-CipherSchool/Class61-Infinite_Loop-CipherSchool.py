f2ab56167d05ee189ffe260652bf03ad67269673
Aaryan <119004779+aaryankashyap@users.noreply.github.com>
2022-11-30 14:34:27 +0530

    Add files via upload

---
+++ week1-PYTHON-CipherSchool/Class61-Infinite_Loop-CipherSchool.py
@@ -0,0 +1,14 @@
+# infinite loop 
+# while True:
+#     print("hey")
+i=0
+while i<=10:
+    print("hello world",i)
+    i+=1
+
+
+
+
+#True 
+#False
+ 
---
+++ week1-PYTHON-CipherSchool/Class62-For_Loop-CipherSchool.py
@@ -0,0 +1,3 @@
+# For Loop 
+for i in range(1,10):
+    print("hello",i)
